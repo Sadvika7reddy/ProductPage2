f83de5c5b226483710ddee4617044ed2d900d025
sadvika Amanaganti <sadvika.25.1@mountblue.tech>
2023-08-12 00:27:13 +0530

    added cart details

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Cart=require('../models/cart')
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
@@ -12,8 +12,10 @@ exports.getProducts = (req, res, next) => {
 exports.getProductDetail=(req,res,next)=>{
     const prodId=req.params.productId;
     Product.findId(prodId,(product)=>{
+      
       res.render('shop/product-detail',{product:product,
         pageTitle:product.title,
+
         path:'/products'
       });
     })
@@ -36,6 +38,13 @@ exports.getCart = (req, res, next) => {
     pageTitle: 'Your Cart'
   });
 };
+exports.postCart=(req,res,next)=>{
+  const prodId=req.body.productId;
+  Product.findId(prodId,product=>{
+    Cart.addProducts(prodId,product.price)
+  })
+  res.redirect('/cart')
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
